7037533afe76e0e8c6d9c375de5a223617122ef3
PaingZinYeMaung <paingzinyemaung.pgo@gmail.com>
2026-01-03 00:13:01 +0630

    js functions added to utterance

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const button = document.querySelector('button');
-const text = document.querySelector('textarea');
+const button = document.querySelector("button");
+const textarea = document.querySelector("textarea");
 
-function TextToSpeech() {
-    const synth = window.speechSynthesis();
-    const utterance = new SpeechSynthesisUtterance(text);
-    synth.speak(utterance);
+const textToSpeech = () => {
+    const synth = window.speechSynthesis;
+    const text = textarea.value;
+    const isSpeaking = true;
 
-}
-button.addEventListener('clicked', TextToSpeech);
+    if(!synth.speaking && text) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        synth.speak(utterance);
+    }
+
+    if(text.length > 50) {
+        if(synth.speaking && isSpeaking) {
+            button.innerText = "Pause";
+            synth.resume();
+            isSpeaking = false;
+        }else {
+            button.innerText = "Resume";
+            synth.pause();
+            isSpeaking = true;
+        }
+    }else {
+        isSpeaking = false;
+        button.innerText = "Speaking";
+    }
+
+    setInterval( () => {
+        if(!synth.speaking && !isSpeaking) {
+            isSpeaking = true;
+            button.innerText = "Convert To Speech";
+        }
+    })
+};
+
+button.addEventListener("click", textToSpeech);
